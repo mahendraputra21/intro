2398ad0fd9532244602a5888dbd07dec39b4fdbd
Dewa Mahendra <31196162+mahendraputra21@users.noreply.github.com>
2021-07-21 21:24:58 +0800

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+const home = $('.home');
+home.hide();
+home.delay(300).fadeIn(300);
+
+/* Open */
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  animateOut: 'slideOutDown',
+  animateIn: 'flipInX',
+  items: 1,
+  margin: 30,
+  stagePadding: 30,
+  smartSpeed: 450,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: false,
+    },
+  },
+});
+
+$('.owl-prev').html('<i class="fas fa-arrow-left"></i>');
+$('.owl-next').html('<i class="fas fa-arrow-right"></i>');
